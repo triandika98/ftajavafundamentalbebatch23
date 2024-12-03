683d3e9d4f3ab3fd06c62d844b9ecca2e64c3356
triandika98 <febbytriandika@gmail.com>
2024-12-03 10:18:22 +0700

    mencoba commit

---
+++ src/main/java/com/juaracoding/Main.java
@@ -0,0 +1,12 @@
+package com.juaracoding;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("my name is Andika");
+        System.out.println("thanks for all, Java");
+
+
+    }
+}
